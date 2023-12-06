2d7b345a1d758a937929d5e68e29033c00ffd9ef
vedant527 <vedant.bhat527@gmail.com>
2023-12-05 22:14:40 -0500

    copies link when contact button clicked

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,12 +21,22 @@ const socials = [
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:vedant.bhat527@gmail.com",
+		address: "vedant.bhat527@gmail.com",
 		label: "Mail",
-		handle: "email me",
+		handle: "vedant.bhat527@gmail",
 	}
 ];
 
+
+
 export default function Contact() {
+	const handleCopyToClipboard = (email: string) => {
+		navigator.clipboard.writeText(email)
+		  .catch((error) => {
+			console.error('Error copying to clipboard:', error);
+		  });
+	}
+	
 	return (
 		<div className="bg-gradient-to-bl">
 			<Navigation />
@@ -41,6 +51,7 @@ export default function Contact() {
 							<Link
 								href={s.href}
 								target="_blank"
+								onClick={() => s.address ? handleCopyToClipboard(s.address) : handleCopyToClipboard(s.href)}
 								className={`p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16`}>
 								<span
 									// className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
