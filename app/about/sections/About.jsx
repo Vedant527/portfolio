e4e2d868a8f54c7c84dfc19d89a95d4b9d17bdc2
vedant527 <vedant.bhat527@gmail.com>
2023-12-13 15:10:02 -0500

    sets up temp text for about page

--- app/about/sections/About.jsx
+++ app/about/sections/About.jsx
@@ -16,25 +16,13 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| Temp" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl"
       >
-        <span className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">Metaverse</span> is a new
-        thing in the future, where you can enjoy the virtual world by feeling
-        like it's really real, you can feel what you feel in this metaverse
-        world, because this is really the{' '}
-        <span className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-          madness of the metaverse
-        </span>{' '}
-        of today, using only{' '}
-        <span className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">VR</span> devices you can
-        easily explore the metaverse world you want, turn your dreams into
-        reality. Let's{' '}
-        <span className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">explore</span> the madness
-        of the metaverse by scrolling down
+        <span className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">Temp</span> Temp
       </motion.p>
     </motion.div>
   </section>
