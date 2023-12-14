724fd94eeb17a95c365232bf83cf18c814a791db
vedant527 <vedant.bhat527@gmail.com>
2023-12-14 12:26:10 -0500

    adds lines to contact cards

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -50,10 +50,9 @@ export default function Contact() {
 								onClick={() => s.address ? handleCopyToClipboard(s.address) : handleCopyToClipboard(s.href)}
 								className={`p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16`}>
 								<span
-									// className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-200 via-zinc-400 to-transparent"
 									aria-hidden="true"
 								/>
-								{/* <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange"> */}
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-slate-50 drop-shadow-orange">
 									{s.icon}
 								</span>{" "}
