7fa0c15b82843f065d1a7af18d98ce9dbd295eb2
vedant527 <vedant.bhat527@gmail.com>
2023-12-14 12:37:59 -0500

    checks for device type before rendering interests

--- app/about/sections/Interests.tsx
+++ app/about/sections/Interests.tsx
@@ -4,9 +4,30 @@ import React from "react";
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../../../util/motion';
+import { useEffect, useState } from "react";
 
 
 const Interests = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check if the window width is less than a certain threshold (e.g., 768px)
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener to check on window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className={`${styles.paddings} relative z-10`}>
       <motion.div
@@ -24,13 +45,23 @@ const Interests = () => {
           <div className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl md:gap-4 md:py-4 lg:pb-4 md:p-4 m-4">
             Topics I am interested in:
             <span className="text-zinc-400 inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
-              <ul className="block animate-text-slide text-left leading-tight [&_li]:block">
-                <li>Low-Level Programming and Parallelization</li>
-                <li>Natural Language Processing and Applications of LLMs</li>
-                <li>High Performance Computing</li>
-                <li>Computer Vision and Real-Time Image Segmentation</li>
-                <li aria-hidden="true">Low-Level Programming and Parallelization</li>
-              </ul>
+              { !isMobile ? 
+                (<ul className="block animate-text-slide text-left leading-tight [&_li]:block">
+                  <li>Low-Level Programming and Parallelization</li>
+                  <li>Natural Language Processing and Applications of LLMs</li>
+                  <li>High Performance Computing</li>
+                  <li>Computer Vision and Real-Time Image Segmentation</li>
+                  <li aria-hidden="true">Low-Level Programming and Parallelization</li>
+                </ul> )
+                :
+                (<ul className="block animate-text-slide text-left leading-tight [&_li]:block">
+                <li>Parallelization</li>
+                <li>NLP</li>
+                <li>HPC</li>
+                <li>Computer Vision</li>
+                <li aria-hidden="true">Parallelization</li>
+              </ul> )
+              }
             </span>
           </div>
         </div>
