be38e54527461461696c1dffbdd0de01c6a3916e
vedant527 <vedant.bhat527@gmail.com>
2023-12-19 15:48:28 -0500

    adds vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
 // import { Analytics } from "./components/analytics";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -51,9 +52,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
-      {/* <head>
-        <Analytics />
-      </head> */}
+      <Analytics />
       <body
         className={`gradient-background ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined}`}
       >
