cdd7ab1637c33a08e65709f561c5f5feb0dcc3b8
vbhat34 <vbhat34@gatech.edu>
2024-11-01 19:49:26 -0700

    blog page skeleton

---
+++ app/blog/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Navigation } from "../components/nav";
+
+
+export default function Blog() {
+    return (
+        <div>
+            <Navigation/>
+            <div className="container flex items-center justify-center min-h-screen px-4 mx-auto justify-items-center">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+
+                </div>
+            </div>
+        </div>
+    )
+}
